da9b207436499e34ea855c44b89acc5015497d01
Emrullah Cirit <emrullahcirit20@gmail.com>
2025-03-13 16:53:25 +0300

    dockerifle

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.141.70.173';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = 'test1234';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
